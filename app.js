d14fe3325ad20546461ed8b333da8d7b0bb18c30
tejanhu <tejanahmedhu@gmail.com>
2023-04-28 02:27:34 +0100

    Updated code

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ function computerPlay(){
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const pc_score_display = document.getElementById("pc-score");
+const player_score_display = document.getElementById("player-score");
+const rps_outcome = document.getElementById("rps-outcome");
 
 var rock_clicked = false;
 var paper_clicked = false;
@@ -31,6 +34,7 @@ function playerPlay(){
        }
        playRound(playRound(player_selection, pc_selection));   
     });
+
     paper.addEventListener("click", function(){
         paper_clicked = true;
         if(paper_clicked){
@@ -38,6 +42,7 @@ function playerPlay(){
         }
         playRound(playRound(player_selection, pc_selection));   
     });
+
     scissors.addEventListener("click", function(){
         scissors_clicked = true;
         if(scissors_clicked){
@@ -45,93 +50,93 @@ function playerPlay(){
         }
         playRound(playRound(player_selection, pc_selection));   
     });
-    
 }
 
 function playRound(player_selection, pc_selection){
     var msg = "";
-
+    
     if(pc_selection === ("Paper") && player_selection === ("Rock")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         pc_score+=5;
         msg = "You lost! Paper beats Rock!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Rock") && player_selection === ("Paper")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         msg = "You won! Paper beats Rock!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Scissors") && player_selection === ("Paper")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         pc_score+=5;
         msg = "You lost! Scissors beats Paper!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Paper") && player_selection === ("Scissors")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         msg = "You won! Scissors beats Paper!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Rock") && player_selection === ("Scissors")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         pc_score+=5;
         msg = "You lost! Rock beats Scissors!";
+        rps_outcome.textContent = msg;
     }
     else if(pc_selection === ("Scissors") && player_selection === ("Rock")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         msg = "You won! Rock beats Scissors!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Rock") && player_selection === ("Rock")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         pc_score+=5;
         msg = "It's a tie! You both have a Rock!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Scissors") && player_selection === ("Scissors")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         pc_score+=5;
         msg = "It's a tie! You both have Scissors!";
+        rps_outcome.textContent = msg;
     }
+
     else if(pc_selection === ("Paper") && player_selection === ("Paper")){
         console.log("You chose: " + player_selection);
         console.log("The PC chose: " + pc_selection);
         player_score+=5;
         pc_score+=5;
         msg = "It's a tie! You both have Paper!";
+        rps_outcome.textContent = msg;
     }
-    console.log(msg);
+    
+    pc_score_display.textContent = pc_score;
+    player_score_display.textContent = player_score;
+
+    if(pc_score == 5 || player_score == 5){
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    }
 }
 
-// function game(){
-   
-//         if(pc_score > player_score){
-//             console.log("The winner is The PC!");
-//         }
-//         else if(player_score > pc_score){
-//             console.log("You're the winner!");
-//         }
-//         else if(player_score == pc_score){
-//             console.log("You both are winners!")
-//         }
-//         console.log("PC SCORE: " + pc_score) 
-//         console.log("PLAYER SCORE: " + player_score) 
-       
-//        else{
-//         playerPlay();
-//         computerPlay();
-//         console.log(playRound(player_selection, pc_selection));
-//        }
-    
-// }
-
-// console.log(game());
 playerPlay();
