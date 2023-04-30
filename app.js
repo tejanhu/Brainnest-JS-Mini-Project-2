82f3601d4443eb9f5594ae3de4666c3ff6372bc0
Hussein Tejan <tejanahmedhu@gmail.com>
2023-04-30 13:31:08 +0100

    testing change

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ var player_score = 0;
 var pc_score = 0;
 
 function computerPlay(){
-    pc_choice = Math.random();
-    if (pc_choice < 0.5) {
-        pc_choice = "Rock";
-    } else if(pc_choice <= 1.5) {
-        pc_choice = "Paper";
-    } else {
-        pc_choice = "Scissors";
-    }
-    return pc_selection = pc_choice;
+    var tools = ["rock", "paper", "scissors"];
+    randomise_tools = tools[Math.floor(Math.random()*(tools.length))];
+    console.log("The Computer chose to play: " + randomise_tools);
+    return pc_selection = randomise_tools;
 }
 
 const rock = document.getElementById("rock");
